6d7ea31d55c74679486e1d5c6a535d86dbc92ef5
Sanhil143 <sanhilrai143@gmail.com>
2024-03-11 23:52:38 +0530

    update env & indentation

--- src/aws/aws-connection.js
+++ src/aws/aws-connection.js
@@ -1,30 +1,26 @@
-const aws = require("aws-sdk")
+const aws = require("aws-sdk");
 
 aws.config.update({
-  accessKeyId: "AKIAY3L35MCRZNIRGT6N",
-  secretAccessKey: "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",
-  region: "ap-south-1"
-})
+  accessKeyId: process.env.Aws_accessKey,
+  secretAccessKey: process.env.Aws_secretKey,
+  region: process.env.Aws_region,
+});
 
-exports.uploadFile= async ( file) =>{
-return new Promise( function(resolve, reject) {
-let s3= new aws.S3({apiVersion: '2006-03-01'});
+exports.uploadFile = async (file) => {
+  return new Promise(function (resolve, reject) {
+    let s3 = new aws.S3({ apiVersion: "2006-03-01" });
 
-  var uploadParams= {
+    var uploadParams = {
       ACL: "public-read",
-      Bucket: "classroom-training-bucket",  //HERE
-      Key: "project5/" + file.originalname, //HERE 
-      Body: file.buffer
+      Bucket: process.env.Aws_bucket,
+      Key: Date.now + file.originalname,
+      Body: file.buffer,
+    };
 
-  }
-
-s3.upload( uploadParams, function (err, data ){
-
- if(err) return reject({"error": err})
-   console.log("file uploaded succesfully")
-    return resolve(data.Location)
-
-  })
-
- })
-}
+    s3.upload(uploadParams, function (err, data) {
+      if (err) return reject({ error: err });
+      console.log("file uploaded succesfully");
+      return resolve(data.Location);
+    });
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const multer = require('multer')
-const cors = require('cors')
-const router = require("./routers/router")
-mongoose.set('strictQuery', true)
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const cors = require("cors");
+const router = require("./routers/router");
+require("dotenv").config();
+mongoose.set("strictQuery", true);
 
-const app = express()
-app.use(express.json())
-app.use(multer().any())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(multer().any());
+app.use(cors());
 
-mongoose.connect("mongodb+srv://sanhil143:raisahab12345@sanhildb.kk3knyj.mongodb.net/group1Database")
-.then(() => console.log("mongoDB is connected"))
-.catch((error) => console.error(error))
+mongoose
+  .connect(process.env.Mongo_Url)
+  .then(() => console.log("mongoDB is connected"))
+  .catch((error) => console.error(error));
 
+app.use("/", router);
 
-app.use('/', router)
-
-app.listen(3000, () => {
-      console.log("Express app running on port " + 3000)
-})
+app.listen(process.env.Port, () => {
+  console.log("Express app running on port " + process.env.Port);
+});
